94e81e43e74657505839a5074346b191a1b22d72
Katerina Noteva <knoteva@gmail.com>
2019-04-02 14:16:41 +0300

    Katerina

---
+++ Technology Fundamentals with C#/02. Data-Types-and-Variables-More-Exercises/04. Refactoring Prime Checker/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _04._Refactoring_Prime_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= num; i++)
+            {
+                bool prime = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine("{0} -> {1}", i, prime.ToString().ToLower());
+            }
+
+        }
+    }
+}
