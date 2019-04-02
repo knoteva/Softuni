be0d223e9b4765ef92c985820fc02d8e4cfed466
Katerina Noteva <knoteva@gmail.com>
2019-04-02 14:24:39 +0300

    Katerina

---
+++ Technology Fundamentals with C#/03.LabIntroandBasicSyntax/1_StudentInformation/8.Divisibleby3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _8.Divisibleby3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 3; i < 100; i+=3)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
