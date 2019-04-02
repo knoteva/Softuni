b2b62e765c40e2e8916ac8eabf67e50374fa6697
Katerina Noteva <knoteva@gmail.com>
2019-04-02 14:22:09 +0300

    Katerina

---
+++ Technology Fundamentals with C#/02. Lab_DataTypesandVariables/04. Centuries to Minutes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+
+namespace _04._Centuries_to_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int centuries = int.Parse(Console.ReadLine());
+            double years = centuries * 100;
+            double days = years * 365.2422;
+            int hours = (int)days * 24;
+            BigInteger minutes = hours * 60;
+            Console.WriteLine($"{centuries} centuries = {years} years = {(int)days} days = {hours} hours = {minutes} minutes");
+        }
+    }
+}
---
+++ Technology Fundamentals with C#/02. Lab_DataTypesandVariables/05. Special Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                sum = i.ToString().Sum(c => c - '0');
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+                else {
+                    Console.WriteLine($"{i} -> False");
+                }
+            }
+        }
+    }
+}
