60b6f8d2b8441c1a93f8c05824c3a8eb77902a12
Katerina Noteva <knoteva@gmail.com>
2019-05-19 13:44:14 +0300

    Add

---
+++ Courses/PHP Web Basics - май 2019/07. PHP-MySQL-Introduction.pptx/querry.sql
@@ -0,0 +1,26 @@
+-- 01. Create Database 
+
+	CREATE DATABASE softun
+	
+	
+-- 02. Create Table 
+
+	CREATE TABLE students
+	(
+	    Id int NOT NULL,
+	    FirstName varchar(50),
+	    LastName varchar(50),
+		Student number int,
+		Phone varchar(50),
+		Date of record varchar(50),
+		Date of last data change varchar(50),
+		Is student is active or not varchar(50),
+	)
+
+
+-- 03. Drop Table 
+
+	drop table students
+	
+-- 04. Drop Database 
+	DROP DATABASE softuni
