fbdce11daa7f99a3b6e0ce0b29bbb3604b168707
Katerina Noteva <knoteva@gmail.com>
2019-05-21 16:34:34 +0300

    change

--- Courses/PHP Web Basics - май 2019/08. PHP-MySQL-Introduction- Exercise/querry.sql
+++ Courses/PHP Web Basics - май 2019/08. PHP-MySQL-Introduction- Exercise/querry.sql
@@ -267,5 +267,4 @@ USE firstheididb
 	SELECT country_name, country_code, 
 	IF (currency_code = 'EUR', 'Euro', 'Not Euro') AS currency 
 	FROM countries
-	WHERE currency_code IS NOT NULL
 	ORDER BY country_name;
