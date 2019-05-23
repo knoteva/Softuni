2ef6776967b77b4824783a16c8287b3b930e1875
Katerina Noteva <knoteva@gmail.com>
2019-05-23 10:35:07 +0300

    Change

---
+++ Courses/Technology Fundamentals with C#/10. ExerciseArrays/10. MoreExerciseArrays/01. Encrypt Sort and Print Array/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace More_Ex_Encrypt_Sort_Print_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] name = new string[n];
+            int vowelSum = 0;
+            int consonantSum = 0;
+            int sum = 0;
+            List<int> nums = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                name[i] = Console.ReadLine();
+                             
+            }
+            for (int i = 0; i < n; i++)
+            {
+                
+
+                foreach (var s in name[i])
+                {
+                    int length = name[i].Length;
+                    if (s == 'a' || s == 'e' || s == 'o' || s == 'u' || s == 'i' || s == 'A' || s == 'E' || s == 'O' || s == 'U' || s == 'I')
+                    {
+                        vowelSum = s * length;
+                        sum += vowelSum;
+                    }
+                    else
+                    {
+                        consonantSum = s / length;
+                        sum += consonantSum;
+                    }
+
+                }       
+                nums.Add(sum);
+                sum = 0;
+            }
+            nums.Sort();
+            foreach (var num in nums)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
