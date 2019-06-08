3cd842bf799164ef258e1df17e5f503d82aa72da
Katerina Noteva <knoteva@gmail.com>
2019-06-08 12:25:24 +0300

    change

---
+++ Courses/PHP Web Basics - май 2019/06. ExercisesHTTPandHTML/03. Sum Two Numbers/03. Sum Two Numbers.php
@@ -0,0 +1,19 @@
+<?php
+if(isset($_GET['num1']) && isset($_GET['num2'])) {
+    $num1 = htmlspecialchars($_GET['num1']);
+    $num2 = htmlspecialchars($_GET['num2']);
+    $sum = $num1 + $num2;
+    echo "$num1 + $num2 = $sum";
+}
+?>
+<form>
+    <div>First Number:</div>
+    <input type="number" name="num1" />
+    <div>Second Number:</div>
+    <input type="number" name="num2" />
+    <div><input type="submit" /></div>
+</form>
+
+
+
+
